bb8f4330c0f23d2a8cae23f0c826ba5ffd8ce1fc
TakanariShimbo <takanari.shimbo@gmail.com>
2025-07-01 08:29:53 +0900

    chore: release version 0.2.0

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ import { TOOLS, TOOL_HANDLERS } from "./tools/index.js";
 const server = new Server(
   {
     name: "mermaid-live-server",
-    version: "0.1.3",
+    version: "0.2.0",
   },
   {
     capabilities: {
